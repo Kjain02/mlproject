8b6376d4b75dd15eb6237784bc9635982282ccc6
Kjain02 <jainkartik95410@gmail.com>
2024-02-13 19:54:27 +0530

    model training and hyperparameter tuning

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -11,6 +11,7 @@ from src.logger import logging
 import os
 from src.utils import save_object
 
+@dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path= os.path.join("artifacts", "preprocessor.pkl")
 
